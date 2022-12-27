ac163f4a671d22dacaa5d4ca9a8f7e09c3ef134b
Nelson Ramirez <xhyskoax@gmail.com>
2022-12-27 20:20:51 -0300

    Agregamos nueva funcionalidad que validad edad de los usuarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var rut = document.getElementById("rut");
+//var nombre = document.getElementById("nombre");
+//var apellido = document.getElementById("apellido");
+//var rut = document.getElementById("rut");
 var formulario = document.getElementById("form-clientes");
-var contador = 0;
 var cuerpoTabla = document.getElementById("cuerpo-tabla");
 var registro = "";
+let contador = 0;
 
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
-  contador++;
-  registro += `
+
+  /* EVALUAMOS SI EL CLIENTE ES MAYOR DE EDAD (<=18 AÑOS) */
+  let edadCliente = parseInt(edad.value);
+  //console.log(typeof edadCliente);
+
+  if (edadCliente >= 18) {
+    contador++;
+    registro += `
             <tr>
               <th scope="row">${contador}</th>
               <td>${nombre.value}</td>
               <td>${apellido.value}</td>
               <td>${rut.value}</td>
-            </tr>
+              <td>${edad.value}</td>
+            </tr>    
     `;
 
-  cuerpoTabla.innerHTML = registro;
-  formulario.reset();
+    cuerpoTabla.innerHTML = registro;
+    formulario.reset();
+  } else {
+    alert(
+      "Atención! el cliente no tiene la edad requerida por sistema (18 años.)"
+    );
+  }
 });
